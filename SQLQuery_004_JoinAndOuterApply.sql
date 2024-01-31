7595e41ef3e83c3eadacd937ab34efbec40374d2
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-01-31 17:38:55 +0100

    Join and Outer Apply

---
+++ SQLQuery_004_JoinAndOuterApply.sql
@@ -0,0 +1,52 @@
+USE [DotNetCourseDatabase]
+GO
+
+/*CREATE PROCEDURE TutorialAppSchema.spUsers_Get*/
+ALTER PROCEDURE TutorialAppSchema.spUsers_Get
+	@UserId INT = NULL
+AS
+BEGIN
+
+    -- SELECT 
+    --     [UserJobInfo].[Department],
+    --     AVG([UserSalary].[AvgSalary]) AvgSalary
+    -- FROM TutorialAppSchema.Users AS Users
+    --     LEFT JOIN TutorialAppSchema.UserSalary AS UserSalary
+    --         ON UserSalary.UserId = Users.UserId
+    --     LEFT JOIN TutorialAppSchema.UserJobInfo AS UserJobInfo
+    --         ON UserJobInfo.UserId = Users.UserId
+    -- GROUP BY [UserJobInfo].[Department]
+
+    SELECT 
+        [Users].[UserId],
+        [Users].[FirstName],
+        [Users].[LastName],
+        [Users].[Email],
+        [Users].[Gender],
+        [Users].[Active],
+        [UserSalary].[Salary],
+        [UserJobInfo].[Department],
+        [UserJobInfo].[JobTitle],
+        AvgSalary.AvgSalary
+    FROM TutorialAppSchema.Users AS Users
+        LEFT JOIN TutorialAppSchema.UserSalary AS UserSalary
+            ON UserSalary.UserId = Users.UserId
+        LEFT JOIN TutorialAppSchema.UserJobInfo AS UserJobInfo
+            ON UserJobInfo.UserId = Users.UserId
+        OUTER APPLY (
+            SELECT 
+                AVG([UserSalary2].[Salary]) AvgSalary
+            FROM TutorialAppSchema.Users AS Users
+                LEFT JOIN TutorialAppSchema.UserSalary AS UserSalary2
+                    ON UserSalary2.UserId = Users.UserId
+                LEFT JOIN TutorialAppSchema.UserJobInfo AS UserJobInfo2
+                    ON UserJobInfo2.UserId = Users.UserId
+            WHERE [UserJobInfo2].[Department] = [UserJobInfo].[Department]
+            GROUP BY [UserJobInfo2].[Department]
+        ) AS AvgSalary
+		WHERE Users.UserId = ISNULL(@UserId, Users.UserId)
+END
+GO
+
+EXEC [TutorialAppSchema].spUsers_Get
+GO
