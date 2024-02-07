63c535a3bbb02772e033c4cbd50d8ffcb8a52ec6
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-02-07 12:14:54 +0100

    Ispravljena greska u Upsert-u

--- SQLQuery_006_UserUpsert.sql
+++ SQLQuery_006_UserUpsert.sql
@@ -11,31 +11,33 @@ CREATE OR ALTER PROCEDURE TutorialAppSchema.spUser_Upsert
 AS
     BEGIN
     IF EXISTS (SELECT * FROM TutorialAppSchema.Users WHERE UserId = @UserId)
-        IF EXISTS (SELECT * FROM TutorialAppSchema.Users WHERE Email = @Email)
-            BEGIN
-            UPDATE TutorialAppSchema.Users SET
-                [FirstName] = @FirstName,
-                [LastName] = @LastName,
-                [Email] = @Email,
-                [Gender] = @Gender,
-                [Active] = @Active
-            WHERE UserId = @UserId
-            END
+        BEGIN
+        UPDATE TutorialAppSchema.Users SET
+            [FirstName] = @FirstName,
+            [LastName] = @LastName,
+            [Email] = @Email,
+            [Gender] = @Gender,
+            [Active] = @Active
+        WHERE UserId = @UserId
+        END
     ELSE
         BEGIN
-        INSERT INTO TutorialAppSchema.Users(
-            [FirstName],
-            [LastName],
-            [Email],
-            [Gender],
-            [Active]
-        ) VALUES (
-            @FirstName,
-            @LastName,
-            @Email,
-            @Gender,
-            @Active
-        )
+        IF NOT EXISTS (SELECT * FROM TutorialAppSchema.Users WHERE Email = @Email)
+            BEGIN
+            INSERT INTO TutorialAppSchema.Users(
+                [FirstName],
+                [LastName],
+                [Email],
+                [Gender],
+                [Active]
+            ) VALUES (
+                @FirstName,
+                @LastName,
+                @Email,
+                @Gender,
+                @Active
+            )
+            END
         END
     END
 GO
